bf36f46a41ed84ee7bb0a15c3faf3c32aa84cb0e
FroggSaron <157082978+FroggSaron@users.noreply.github.com>
2024-01-30 15:02:33 -0600

    Added extra annotations to search.py and annotated the mysteryScript.py

--- mysteryScript.py
+++ mysteryScript.py
@@ -1,7 +1,12 @@
+# Asks for the User's name and assigns it to a variable
 name = input("What is your name? ")
-
+# Prints the start of the celebratory message
 print("Hooray for")
+# Creates a loop that will repeat fo each letter in the name inputted
 for c in name:
+    # Prints one letter to the screen
     print(c)
+    # Creates a space for the next letter to go under
     print()
+# Finishes the program with an exclamation mark
 print("!")
--- search.py
+++ search.py
@@ -4,19 +4,20 @@ to search the area, and figures out how many circles the rescue drone would need
 @author: Sam Jackson (sjackso2@macalester.edu)
 """
 import math
-# Asks user the dimensions of the rectangular area the drone will search
-width = input("What is the width in meters of the search area:")
-length = input("What is the length in meters of the search area:")
-width = int(width)
-length = int(length)
+# Asks user the dimensions of the rectangular area the drone will search and assigns it to an integer variable
+width = int(input("What is the width in meters of the search area:"))
+length = int(input("What is the length in meters of the search area:"))
+
 # Calculates the centerpoint of the search area
 centerpointx = width / 2
 centerpointy = length / 2
 # Finds the radius of the circle that the drone uses
 diameter = math.sqrt((centerpointx ** 2) + (centerpointy ** 2))
 radius = diameter / 2
+# Prints the radius
 print(radius)
 # Finds how many circles the drone will have to make rounded to the largest integer
 circleNum = radius / 5
 circleNum = math.ceil(circleNum)
+# Prints the number of circles
 print(circleNum)
